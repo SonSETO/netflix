45a9ca981223758a07c860970a35d851dbfda6ba
yesthis9090@gmail.com <yesthis9090@gmail.com>
2024-11-27 23:57:07 +0900

    Lightsail

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -19,7 +19,7 @@ import { CommonService } from 'src/common/common.service';
 import { join } from 'path';
 import { rename } from 'fs/promises';
 import { User } from 'src/user/entity/user.entity';
-import { preconnect } from 'next/dist/server/app-render/entry-base';
+
 import { MovieUserLike } from './entity/movie-user-like.entity';
 import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
 
